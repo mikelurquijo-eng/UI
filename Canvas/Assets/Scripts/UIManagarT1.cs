34ea80a1ffeb000aff75e22eb7e5e6d050f63132
murkri229 <mikel.urquijo@opendeusto.es>
2026-05-07 09:27:08 +0200

    New Project Created

---
+++ Canvas/Assets/Scripts/UIManagarT1.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class UIManagarT1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void PlayGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ButtonScene");
+    }
+}
+
+
